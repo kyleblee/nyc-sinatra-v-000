a6ccc0d2c7e934223a0b3d81914c5d4eb8222a90
kyleblee <hello.kylelee@gmail.com>
2017-11-09 15:29:59 -0500

    lab complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < Sinatra::Base
-
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "nyc_sinatra_secret"
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
 end
